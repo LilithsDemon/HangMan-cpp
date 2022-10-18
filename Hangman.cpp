8ee3890ab37acc389a055f2876f57dd510cbbc5b
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-10-18 11:30:25 +0100

    Completed project

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,31 +1,3 @@
-/*
-3 difficuties
-easy:
-    8 lives
-Medium:
-    6 lives
-    "chosen", "fisherman", "attention", "pixel", "zombie", "rhythm", "jumper", "jogger"
-Hard:
-    4 lives
-    "crypt", "embezzle", "voodoo", "oxidize", "wyvern", "sphinx", "hazard", "blizzard"
-
-Input
-
-· Start game
-· Letter guesses (i.e., “a”, “b”, “c”)
-· Quit game or Restart game
-
-When a user wins/loses, 
-the program should ask them if they want to play again. 
-There should be an option to quit the program at this point. You should also include a start menu before the user begins the game.
-
-Output
-· Welcome to the program
-· Word with _ as missing letters
-· How many lives does the user have left?
-· Would you like to play again?
-*/
-
 #include <iostream>
 #include <vector>
 #include <ctime>
@@ -57,7 +29,7 @@ void printMenu()
 {
     std::cout << std::endl << "Welcome to Hangman: " << std::endl
         << "Would you like to: " << std::endl
-        << "1. Play hangman on easy diffuclt, " << std::endl
+        << "1. Play hangman on easy diffuclty, " << std::endl
         << "2. Play hangman on medium difficulty, " << std::endl
         << "3. Play hangman on hard difficulty. " << std::endl
         << "4. Quit" << std::endl
@@ -150,6 +122,7 @@ void playGame(std::string word_to_guess, int lives)
         if (lives == 0)
         {
             dead = true;
+            std::cout << "\nSorry you lost the word was: " << word_to_guess << "\n";
             break;
         }
         already_guessed = false;
@@ -171,26 +144,34 @@ void playGame(std::string word_to_guess, int lives)
                 already_guessed = true;
             }
         }
-        guessed_letters.push_back(letter_guess);
-        if(letterInWord(letter_guess, word_to_guess) == true)
+        if(!already_guessed)
         {
-            std::cout << "\nThat letter is in the word, well done\n\n";
-            std::string current_string = convertString(word_to_guess, guessed_letters);
-            word_found = true;
-            for (int i = 0; i < current_string.length(); i++)
+            guessed_letters.push_back(letter_guess);
+            if(letterInWord(letter_guess, word_to_guess) == true)
             {
-                if(current_string[i] == "_")
+                std::cout << "\nThat letter is in the word, well done\n\n";
+                std::string current_string = convertString(word_to_guess, guessed_letters);
+                std::string place_holder = "_";
+                word_found = true;
+                for (int i = 0; i < current_string.length(); i++)
                 {
-                    word_found = false;
+                    if(current_string[i] == place_holder[0])
+                    {
+                        word_found = false;
+                    }
+                }
+                if (word_found == true)
+                {
+                    std::cout << "Well done, you got the word: " << word_to_guess
+                              << " you won with " << lives << ": lives left\n";
+                    break;
                 }
             }
-            std::cout << "Well done, you got the word: " << word_to_guess
-                      << " you won with " << lives << ": lives left\n";
-        }
-        else
-        {
-            std::cout << "\nThat letter is not in the word \n\n";
-            lives--;
+            else
+            {
+                std::cout << "\nThat letter is not in the word \n\n";
+                lives--;
+            }
         }
     }
 }
@@ -200,6 +181,7 @@ int main()
     printStart();
     int option = 0;
     bool play = true;
+    std::string play_again;
     std::string word = "";
     while(play == true)
     {
@@ -239,6 +221,17 @@ int main()
                 std::cout << "\nSorry that is not a valid response, please retry";
                 break;
             }
+        }
+        std::cout << "Would you like to play again (y/n): ";
+        std::cin >> play_again;
+        if (play_again == "y" || play_again == "yes")
+        {
+            std::cout << "\n";
+        }
+        else
+        {
+            std::cout << "\nThank you for playing hangman :)\n";
+            play = false;
         }
     }
 
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Hangman
 Simple Hangman game using C++ and cli
 Made for midterm assesement
+
+---
+
+In this project there are 3 difficulties
+
+Easy:
+    * 8 lives
+    * "apple", "car", "cheese", "cold", "wave", "boat", "heal", "four"
+Medium:
+    * 6 lives
+    * "chosen", "fisherman", "attention", "pixel", "zombie", "rhythm", "jumper", "jogger"
+Hard:
+    * 4 lives
+    * "crypt", "embezzle", "voodoo", "oxidize", "wyvern", "sphinx", "hazard", "blizzard"
+
+Input
+* Start game
+* Letter guesses (i.e., "a", "b", "c")
+* Quit game or Restart Game
+
+Output
+* Welcome to the program
+* Word with _ as missing letters
+* How many lives does the user have left?
+* Would you like to play again?
+
+When a user wins/loses
+* The program should ask them if they want to play again
+* There should be an option to quit the program at this point
+* There should also be a start menu before the user begins the game
